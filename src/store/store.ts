ec171ea1ee1651defed358f994494d016401b0ea
arybary <l3fljl5ldydy@gmail.com>
2023-11-30 14:23:21 +0200

    delete logger

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import tasksReducer from '../tasks/slice/tasks.slice';
 import paginationReducer from '../tasks/slice/pagination.slice';
-import logger from './logger';
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
     pagination: paginationReducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
 });
 
 export type AppDispatch = typeof store.dispatch;
